f6f327764de1716f23e111a1f3278695b3369e16
gerome-byte <geromeaidan@gail.com>
2022-12-04 06:00:05 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("{:d} argument.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        j = 1
+        for i in range(len(sys.argv[1:])):
+            print("{:d}: {}".format(j, sys.argv[j]))
+            j = j + 1
