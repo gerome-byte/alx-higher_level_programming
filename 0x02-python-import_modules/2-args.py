d05ee721feacd91b4fb29c29ebff3d750fb31926
gerome-byte <geromeaidan@gail.com>
2022-12-04 06:12:20 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("{:d} argument.".format(len(sys.argv) - 1))
+        print("{:d} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
         print("{:d} argument:".format(len(sys.argv) - 1))
-        print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[len(sys.argv) - 1]))
+        print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1))
         j = 1
