bc60f64644d4c233ee118ba10feafb5649ba2dd2
gerome-byte <geromeaidan@gail.com>
2022-12-04 11:34:51 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,11 +4,9 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print(1)
         exit(1)
     if len(sys.argv) - 1 != 3 and len(sys.argv) != 1:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print(1)
         exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
@@ -18,21 +16,16 @@ if __name__ == "__main__":
     f = div(a, b)
     if sys.argv[2] == "+":
         print("{:d} + {:d} = {:d}".format(a, b, c))
-        print(0)
         exit(0)
     elif sys.argv[2] == "-":
         print("{:d} - {:d} = {:d}".format(a, b, d))
-        print(0)
         exit(0)
     elif sys.argv[2] == "*":
         print("{:d} * {:d} = {:d}".format(a, b, e))
-        print(0)
         exit(0)
     elif sys.argv[2] == "/":
         print("{:d} / {:d} = {:d}".format(a, b, f))
-        print(0)
         exit(0)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        print(1)
         exit(1)
