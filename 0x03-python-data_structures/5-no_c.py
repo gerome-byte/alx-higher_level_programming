a9a3122ef0914cf5b1e25dfecd63b6984467a4c1
gerome-byte <geromeaidan@gail.com>
2022-12-06 13:03:19 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
         update_string = ''
         for i in my_string:
             if i != C and i != c:
                 update_string += i
-        return my_string
+        return update_string
