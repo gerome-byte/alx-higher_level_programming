afc49b95ff8be8d56e56ec47d3654b6b14959563
gerome-byte <geromeaidan@gail.com>
2022-12-07 03:36:11 -0800

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     my_list.sort()
     x = len(my_list) - 1
-    print("{:d}".format(my_list[x]))
+    return my_list[x]
