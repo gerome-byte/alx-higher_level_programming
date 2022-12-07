6f51b300d1be0a7c682aa2b9f0ac5a5229b05740
gerome-byte <geromeaidan@gail.com>
2022-12-07 11:28:18 -0800

    c- files

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -15,6 +15,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+void reverse_listint(listint_t **head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
