355ec5f100140806b5bdc06c782021e199772c0b
gerome-byte <geromeaidan@gail.com>
2022-12-09 05:24:18 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        x = sorted(dict1.values())
+        x = sorted(a_dictionary.values())
         y = list(x)
         z = y[len(y) - 1]
         for k, v in a_dictionary.items():
