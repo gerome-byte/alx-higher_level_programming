2086da3f21d36b86e5621b35a859fd125cb8b1f8
gerome-byte <geromeaidan@gail.com>
2022-12-09 02:49:23 -0800

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictinary[key]
-        return a_dictionary
-    else:
-        return a_dictionary
+    del a_dictinary[key]
+    return a_dictionariy
