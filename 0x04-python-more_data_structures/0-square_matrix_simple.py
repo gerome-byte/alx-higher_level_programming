756a1ae7be2d4bc345045203ddb07c8050c78f44
gerome-byte <geromeaidan@gail.com>
2022-12-08 22:09:29 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     m = []
     for i in range(len(matrix)):
-        z = list(map(lambda x: pow(x, 2), matrix(i)))
+        z = list(map(lambda x: pow(x, 2), matrix[i]))
         m.append(z)
     return m
