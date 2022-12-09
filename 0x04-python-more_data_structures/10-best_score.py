03282b4fda2dada3a48310da80d17b1318e4533a
gerome-byte <geromeaidan@gail.com>
2022-12-09 05:21:03 -0800

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        x = sorted(dict1.values())
+        y = list(x)
+        z = y[len(y) - 1]
+        for k, v in a_dictionary.items():
+            if v == z:
+                return k
+    else:
+        return None               
